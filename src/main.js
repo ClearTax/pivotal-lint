891fd616a2e02046ff25bcee45a63a94b4f461d7
rajanand02 <rajanand0202@gmail.com>
2019-09-14 18:54:43 +0530

    refactor, add debug logs

--- src/main.js
+++ src/main.js
@@ -2,6 +2,14 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 const axios = require("axios");
 
+const getPivotalId = branch => {
+  const match = branch.match(/\d{9}/);
+  if (match && match.length) {
+    return match[0];
+  }
+  return "";
+};
+
 async function run() {
   try {
     const PIVOTAL_TOKEN = core.getInput("pivotal-token", { required: true });
@@ -11,29 +19,17 @@ async function run() {
       payload: { repository, organization },
       sha
     } = github.context;
-    console.log(github.context)
 
     const repoDetails = {
       owner: organization.login,
       repo: repository.name
     };
 
-    console.log('repoDetails', repoDetails);
-
-
     const request = axios.create({
       baseURL: `https://www.pivotaltracker.com/services/v5`,
       timeout: 2000,
       headers: { "X-TrackerToken": PIVOTAL_TOKEN }
     });
-
-    const getPivotalId = branch => {
-      const match = branch.match(/\d{9}/);
-      if (match && match.length) {
-        return match[0];
-      }
-      return "";
-    };
 
     const getStoryDetails = async storyId => {
       return await request.get(`/stories/${storyId}`).then(res => res.data);
@@ -51,7 +47,7 @@ async function run() {
           labels: [label]
         });
       } catch (error) {
-        console.log(error);
+        core.setFailed(error.message);
       }
     };
 
@@ -69,14 +65,15 @@ async function run() {
         }
         return undefined;
       } catch (error) {
-        console.log(error);
+        core.setFailed(error.message);
       }
     };
 
     const checkPivotal = async () => {
       const { ref } = github.context;
+      core.debug("Checking pivotal id");
       const pivotalId = getPivotalId(ref);
-      console.log("Log: pivotalId ", pivotalId);
+      core.debug("pivotalId -> ", pivotalId);
 
       if (!pivotalId) {
         core.setFailed("Pivotal id is missing in your branch.");
@@ -97,7 +94,7 @@ async function run() {
     };
 
     const projectName = await checkPivotal();
-    console.log("Log: -> projectName ", projectName);
+    core.debug("projectName -> ", projectName);
     if (!projectName) {
       core.setFailed("Could not get project name from the pivotal id");
     }
