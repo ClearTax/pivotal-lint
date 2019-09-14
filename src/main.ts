aa2915dfdbb6dce198bcd22153300a77fd2aa19c
rajanand02 <rajanand0202@gmail.com>
2019-09-14 14:25:58 +0530

    add pivotal id check

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,14 @@ import * as core from '@actions/core';
 import * as github from '@actions/github';
 
 
+const getPivotalId = branch => {
+  const match = branch.match(/\d{9}/);
+  if(match && match.length) {
+    return match[0];
+  }
+  return '';
+}
+
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
@@ -9,13 +17,14 @@ try {
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
-  const { head_branch, pull_requests }= github.context.payload;
-  console.log('Log: pull_requests', pull_requests);
-  console.log('Log: head_branch', head_branch);
-
   console.log(JSON.stringify(github.context, undefined, 2));
+
   const { ref } = github.context;
-  console.log('Log: ref ', JSON.stringify(ref, undefined, 2));
+  const pivotalId = getPivotalId(ref);
+  console.log('Log: pivotalId ', pivotalId );
+  if(!pivotalId) {
+    core.setFailed('Pivotal id is missing in your branch.');
+  }
   core.setFailed('fail to try rerun');
 } catch (error) {
   core.setFailed(error.message);
