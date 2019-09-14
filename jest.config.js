b5be46c2f33e595d70bc76d5a29bb2ca323ff3f9
rajanand02 <rajanand0202@gmail.com>
2019-09-14 16:26:05 +0530

    remove build from action

--- jest.config.js
+++ jest.config.js
@@ -4,8 +4,5 @@ module.exports = {
   testEnvironment: 'node',
   testMatch: ['**/*.test.ts'],
   testRunner: 'jest-circus/runner',
-  transform: {
-    '^.+\\.ts$': 'ts-jest'
-  },
   verbose: true
 }
