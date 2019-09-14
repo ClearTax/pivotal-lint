c3b02f4dabd3a98dcf78786b49169db7fbe2d92d
rajanand02 <rajanand0202@gmail.com>
2019-09-14 20:50:02 +0530

    add jsdoc and remove logs

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 const axios = require("axios");
 
+/**
+ *  Extract pivotal id from the branch name
+ * @param {string} branch
+ */
 const getPivotalId = branch => {
   const match = branch.match(/\d{9}/);
   if (match && match.length) {
@@ -14,13 +18,14 @@ async function run() {
   try {
     const PIVOTAL_TOKEN = core.getInput("pivotal-token", { required: true });
     const GITHUB_TOKEN = core.getInput("github-token", { required: true });
+
+    // it is easier to get from the workflow context
     const headBranch = core.getInput("head-branch", { required: true });
     const client = new github.GitHub(GITHUB_TOKEN);
 
     const {
       payload: { repository, organization, pull_request },
     } = github.context;
-    console.log(github.context);
 
     const repoDetails = {
       owner: organization.login,
@@ -33,14 +38,27 @@ async function run() {
       headers: { "X-TrackerToken": PIVOTAL_TOKEN }
     });
 
+    /**
+     * Get story details based on story id
+     */
     const getStoryDetails = async storyId => {
       return await request.get(`/stories/${storyId}`).then(res => res.data);
     };
 
+    /**
+     * Get project details based on project id
+     * @param {string} projectId
+     */
     const getProjectDetails = async projectId => {
       return await request.get(`/projects/${projectId}`).then(res => res.data);
     };
 
+    /**
+     * Add the specified label to the PR
+     * @param {object} client
+     * @param {string} prNumber
+     * @param {string[]} label
+     */
     const addLabels = async (client, prNumber, label) => {
       try {
         await client.issues.addLabels({
@@ -53,6 +71,9 @@ async function run() {
       }
     };
 
+    /**
+     * Check the pivotal story using the pivotal API
+     */
     const checkPivotal = async () => {
       console.log("Checking pivotal id for ", headBranch);
       const pivotalId = getPivotalId(headBranch);
